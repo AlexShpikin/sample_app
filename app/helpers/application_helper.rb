4f87616b502145a0493db0dc46be0f0745291760
alex <alexei.shpikin@yandex.ru>
2017-09-19 15:10:34 +0300

    placehold for static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+	def full_title(page_title)
+		base_title = "Ruby on Rails Tutorial Sample App"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end		
+	def meta_desc(page_title)
+		base_desc = "Ruby on Rails Tutorial Sample App"
+		if page_title.empty?
+			base_desc
+		else
+			"#{base_desc} #{page_title}"
+		end		
+			
+	end	
 end
