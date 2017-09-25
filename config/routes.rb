eec5c44a2f5f6eb6b16d704d0021c017428a9c11
alex <alexei.shpikin@yandex.ru>
2017-09-25 18:55:31 +0300

    final

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  	resources :users
+  	resources :users do
+      member do
+        get :following, :followers
+      end
+    end
   	resources :sessions, only: [:new, :create, :destroy]
   	resources :microposts, only: [:create, :destroy]
-	root 'static_pages#home'
-	match '/signup',  to: 'users#new',            via: 'get'
-	match '/signin',  to: 'sessions#new',         via: 'get'
-	match '/signout', to: 'sessions#destroy',     via: 'delete'
+    resources :relationships, only: [:create, :destroy]
+	 root 'static_pages#home'
+	 match '/signup',  to: 'users#new',            via: 'get'
+	 match '/signin',  to: 'sessions#new',         via: 'get'
+	 match '/signout', to: 'sessions#destroy',     via: 'delete'
   	match '/home', to: 'static_pages#home', via: 'get'
   	match '/help', to: 'static_pages#help', via: 'get'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
